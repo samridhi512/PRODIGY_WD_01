e6bfea142e22eed82f422afbabf09e8c68e32b57
samridhi512 <146466483+samridhi512@users.noreply.github.com>
2024-07-03 15:07:06 +0530

    Add files via upload

---
+++ task 3/script.js
@@ -0,0 +1,22 @@
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            window.scrollTo({
+                top: target.offsetTop - 50,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+menuToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
